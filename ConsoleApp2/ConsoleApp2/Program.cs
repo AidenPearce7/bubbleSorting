5879de55e164fa9c6cd4ef31ede9d690ca61f7bf
AidenPearce7 <30537758+AidenPearce7@users.noreply.github.com>
2018-08-02 16:31:24 +0200

    Add files via upload

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+            int[] tableToSort = new int[5];
+            for (int i = 0; i <tableToSort.Length; i++)
+            {
+                tableToSort[i] = rand.Next(20);
+                Console.Write(tableToSort[i]+" ");
+            }
+            Console.WriteLine();
+            bool isSorted =false;
+            while (!isSorted)
+            {
+                isSorted = true;
+                Console.WriteLine();
+                for (int i = 0; i < tableToSort.Length; i++)
+                {
+                    int k = i + 1;
+                    if (k==tableToSort.Length)
+                    {
+                        Console.Write(tableToSort[i] + " ");
+                        continue;
+                    }
+                    if (tableToSort[i] > tableToSort[k])
+                    {
+                        int temp = tableToSort[i];
+                        tableToSort[i] = tableToSort[k];
+                        tableToSort[k] = temp;
+                        isSorted = false;
+                    }
+                    Console.Write(tableToSort[i]+" ");
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
